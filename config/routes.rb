61ef2fef761a894ad0e443f95f599bda5db07d80
czying <823698255@qq.com>
2015-08-06 10:29:09 +0800

    modify distrib.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :power, only: [:index] do
     collection do 
       get :ups
+      get :distrib
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
