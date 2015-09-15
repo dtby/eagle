88a1c3fb818d4f708b215de366c8830fc04953e8
yuzixun <yzxun@qq.com>
2015-09-15 16:36:59 +0800

    add room model

---
+++ spec/factories/rooms.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :room do
+    name "MyString"
+  end
+
+end
