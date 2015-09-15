894efab33e4d0cb962f400b32c214c59b0410d40
yuzixun <yzxun@qq.com>
2015-09-15 17:30:16 +0800

    add method for generate data

--- app/models/room.rb
+++ app/models/room.rb
@@ -9,4 +9,26 @@
 #
 
 class Room < ActiveRecord::Base
+
+
+  def self.get_computer_room_list
+    # 名字-> [{系统 -> 设备}, ... {系统 -> 设备}]
+    point_hash = {}
+    datas_to_hash AnalogPoint, point_hash
+    datas_to_hash DigitalPoint, point_hash
+    generate_system point_hash
+  end
+
+  def self.generate_system  point_hash
+    # 机房 => { 系统 => 子系统 => {点 => 数据}
+    point_hash.each do |room, system_hash|
+      room = Room.find_or_create_by(name: room)
+      system_hash.each do |sys_name, sub_systems|
+        sub_systems.each do |sub_system|
+          system = System.find_or_create_by(sys_name: sys_name, sub_system: sub_system.first)
+        end
+      end
+    end
+  end
+
 end
