346fb0233dfea97a9336d9c83b5972de1c695a85
张留刚 <www.zhangyi508@gmail.com>
2015-09-25 11:02:09 +0800

    add menu method

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -26,7 +26,14 @@ class Admin::RoomsController < Admin::BaseController
 	end
 
 	def update
-    
+    if @room.update(room_params)
+    	Menu.update_menus_by_room(@room, params[:list])
+			flash[:notice] = "更新成功"
+			return redirect_to admin_rooms_path
+		else
+			flash[:notice] = "更新失败"
+			return redirect_to admin_rooms_path
+		end
   end
 
 	def show
@@ -34,6 +41,10 @@ class Admin::RoomsController < Admin::BaseController
 	end
 
 	private 
+
+	def room_params
+		params.require(:room).permit(:name, :link)
+	end
 
   def set_room
     @room = Room.where(id: params[:id]).first
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -18,4 +18,23 @@
 class Menu < ActiveRecord::Base
   belongs_to :room
   belongs_to :menuable, polymorphic: true
+
+  # 参数: room,机房；list，菜单［"#{menuable_id}_#{menuable_type}"］］
+  def self.update_menus_by_room(room, menus)
+  	old_menus = room.menu_to_s
+  	add_menus = menus - old_menus
+  	delete_menus = old_menus - menus
+  	
+  	add_menus.each do |m|
+  		menuable_id = m.split('_')[0]
+  		menuable_type = m.split('_')[1]
+  		self.create(room: room, menuable_id: menuable_id, menuable_type: menuable_type)
+  	end
+
+  	delete_menus.each do |m|
+  		menuable_id = m.split('_')[0]
+  		menuable_type = m.split('_')[1]
+  		self.where(room: room, menuable_id: menuable_id, menuable_type: menuable_type).destroy_all
+  	end
+  end
 end
