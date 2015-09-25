659ec535951e5b2a969c798fde495a954c5fe56d
yuzixun <yzxun@qq.com>
2015-09-26 01:38:20 +0800

    add self.inheritance_column is null for process with the type in database

--- app/models/point_state.rb
+++ app/models/point_state.rb
@@ -11,4 +11,9 @@ class PointState < ActiveRecord::Base
   self.table_name = "litop_point"
   self.abstract_class = true
   establish_connection "3droom_db".to_sym
+
+  # 由于源数据库中存在type字段
+  # 在rails中，默认用这个字段来处理继承。
+  # 为防止报错，添加以下语句
+  self.inheritance_column = ""
 end
