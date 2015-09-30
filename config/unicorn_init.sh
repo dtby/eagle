470df6f7640f658d7e915b1323cc0acc1a392cbc
OnlyCZY <853852998@qq.com>
2015-09-30 14:42:20 +0800

    modify unicorn_init

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deploy/eagle_wechat
+APP_ROOT=/home/deploy/eagle
 
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
