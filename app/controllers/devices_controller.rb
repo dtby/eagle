2a393f75b8fe003c1ce6aa8e748b791faed4dd54
fanwei <fwshun8023@gmail.com>
2015-09-30 12:49:16 +0800

    device show

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,6 +1,7 @@
 class DevicesController < BaseController
 
   def show
-    @device = Device.where(id: params[:id]).first
+    @device = Device.includes(:points).where(id: params[:id]).first
+    @exclude_points = @device.pattern.getting_exclude_points
   end
 end
