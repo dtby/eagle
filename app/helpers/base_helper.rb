4f203728c59ec01395f3c795d1da475269226700
fw <fwshun8023@gmail.com>
2015-10-28 11:26:11 +0800

    leaking complete

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -3,4 +3,13 @@ module BaseHelper
     return "---" if exclude_points.present? && exclude_points.include?(point_name) 
     points[point_name]
   end
+
+
+  def circle_color(value)
+    if value == "0"
+      "green"
+    elsif value == "1"
+      "red"
+    end
+  end
 end
