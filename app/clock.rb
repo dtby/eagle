4474ee9522fb29bfc6b2d14e45fc5105862bd8c1
yuzixun <yzxun@qq.com>
2015-12-03 10:59:12 +0800

    add log config

--- app/clock.rb
+++ app/clock.rb
@@ -3,6 +3,14 @@ require './config/boot'
 require './config/environment'
 module Clockwork
 
+  configure do |config|
+    config[:sleep_timeout] = 5
+    config[:logger] = Logger.new("log/clock.log")
+    config[:tz] = 'EST'
+    config[:max_threads] = 15
+    config[:thread] = true
+  end
+
   handler do |job|
     puts "Running #{job}"
   end
