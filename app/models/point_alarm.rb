65c55272a9c35497d127e325b7a588332c172fbb
yuzixun <yzxun@qq.com>
2015-12-03 10:35:27 +0800

    add get state by room

--- app/models/point_alarm.rb
+++ app/models/point_alarm.rb
@@ -21,9 +21,18 @@
 class PointAlarm < ActiveRecord::Base
   belongs_to :point
 
-  def self.get_alarm_point
+  # PointAlarm.get_alarm_point_by_room 1
+  def self.get_alarm_point_by_room room_id
+    devices = Device.by_room room_id
+    return unless devices.present?
+    points = []
+    devices.collect { |device| points.concat device.points.pluck(:point_index)}
+
     point_alarms = {}
-    PointAlarm.where.not(state: 0).collect { |p| point_alarms[p.try(:point).try(:point_index)] = p.state}
+    Point.where(point_index: points).each do |point|
+      state = point.try(:point_alarm).try(:state)
+      point_alarms[point.point_index] = state if state.present? && state != 0
+    end
     point_alarms
   end
   
