17fd576d58233824e5ed953ac82cc587eec20efc
xiaocuixt <cjw624923@126.com>
2015-12-03 15:26:24 +0800

    fix bug

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,6 +1,7 @@
 class DevicesController < BaseController
 
   def show
+    @room = Room.where(id: params[:room_id]).first
     @device = Device.includes(:points).where(id: params[:id]).first
     @points = @device.points_group
     @exclude_points = @device.pattern.getting_exclude_points
