e85ef2bfafbe7276e0000a3c712f965f8b35d658
xiaocuixt <cjw624923@126.com>
2016-01-20 19:59:44 +0800

    fix bug

--- db/seeds/development/systems.seeds.rb
+++ db/seeds/development/systems.seeds.rb
@@ -22,9 +22,8 @@ SubSystem.create!([
   { id: 8, system_id: 2, name: '空调系统' },
   { id: 9, system_id: 2, name: '漏水系统' },
 
-  { id: 10, system_id: 4, name: '消防报警' },
-  { id: 11, system_id: 4, name: '烟感报警' },
-  { id: 12, system_id: 4, name: '氢气检测' }
+  { id: 10, system_id: 4, name: '消防系统' },
+  { id: 11, system_id: 4, name: '氢气检测' }
 ])
 # SubSystem.create!([
 #   { id: 1, system_id: 1, name: '配电系统' },
@@ -58,8 +57,7 @@ Pattern.create!([
   { id: 6, sub_system_id: 8, name: '威图', partial_path: 'cool_sys_weitu' },  #"风冷系统"=>"威图",
   { id: 7, sub_system_id: 9, name: '7053', partial_path: 'leaking_7053' },  # "漏水系统"=>"7053", nil=>nil}
 
-  { id: 8, sub_system_id: 11, name: '烟感', partial_path: 'smoke_7053' }, # "烟感系统"=>"7053",
-  { id: 9, sub_system_id: 10, name: '消防', partial_path: 'fire_7053' },  # "消防系统"=>"7053", nil=>nil}
+  { id: 9, sub_system_id: 10, name: '7053', partial_path: 'fire_7053' },  # "消防系统"=>"7053", nil=>nil}
 
   { id: 10, sub_system_id: 6, name: '普通温湿度', partial_path: 'commen_temp' }, # "温湿度系统"=>"普通温湿度" and "th802",
 ])
