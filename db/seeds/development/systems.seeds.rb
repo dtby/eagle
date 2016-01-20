5488349accb3e06a7a26ba7cf674e3c25c363fa3
xiaocuixt <cjw624923@126.com>
2016-01-20 19:39:52 +0800

    fix bug

--- db/seeds/development/systems.seeds.rb
+++ db/seeds/development/systems.seeds.rb
@@ -57,8 +57,8 @@ Pattern.create!([
   { id: 5, sub_system_id: 8, name: '7053', partial_path: 'air_c_7053' },  # "空调系统"=>"7053",
   { id: 6, sub_system_id: 9, name: '7053', partial_path: 'leaking_7053' },  # "漏水系统"=>"7053", nil=>nil}
 
-  { id: 7, sub_system_id: 11, name: '7053', partial_path: 'fire_7053' }, # "烟感系统"=>"7053",
-  { id: 8, sub_system_id: 10, name: '7053', partial_path: 'fire_7053' },  # "消防系统"=>"7053", nil=>nil}
+  { id: 7, sub_system_id: 11, name: '烟感', partial_path: 'smoke_7053' }, # "烟感系统"=>"7053",
+  { id: 8, sub_system_id: 10, name: '消防', partial_path: 'fire_7053' },  # "消防系统"=>"7053", nil=>nil}
 
   { id: 9, sub_system_id: 6, name: '普通温湿度', partial_path: 'commen_temp' }, # "温湿度系统"=>"普通温湿度" and "th802",
 ])
