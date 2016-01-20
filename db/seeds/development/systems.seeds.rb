d02343a92a74476cadea467a1ca506711a3303c0
xiaocuixt <cjw624923@126.com>
2016-01-20 19:15:38 +0800

    fix seed

--- db/seeds/development/systems.seeds.rb
+++ db/seeds/development/systems.seeds.rb
@@ -55,11 +55,12 @@ Pattern.create!([
 
   { id: 4, sub_system_id: 8, name: '威图列间空调', partial_path: 'weitu' },
   { id: 5, sub_system_id: 8, name: '7053', partial_path: 'air_c_7053' },  # "空调系统"=>"7053",
+  { id: 6, sub_system_id: 9, name: '7053', partial_path: 'leaking_7053' },  # "漏水系统"=>"7053", nil=>nil}
 
-  { id: 6, sub_system_id: 11, name: '7053', partial_path: 'fire_7053' }, # "烟感系统"=>"7053",
-  { id: 7, sub_system_id: 10, name: '7053', partial_path: 'leaking_7053' },  # "消防系统"=>"7053", nil=>nil}
+  { id: 7, sub_system_id: 11, name: '7053', partial_path: 'fire_7053' }, # "烟感系统"=>"7053",
+  { id: 8, sub_system_id: 10, name: '7053', partial_path: 'fire_7053' },  # "消防系统"=>"7053", nil=>nil}
 
-  { id: 8, sub_system_id: 6, name: '普通温湿度', partial_path: 'commen_temp' }, # "温湿度系统"=>"普通温湿度" and "th802",
+  { id: 9, sub_system_id: 6, name: '普通温湿度', partial_path: 'commen_temp' }, # "温湿度系统"=>"普通温湿度" and "th802",
 ])
 
 # Pattern.create!([
