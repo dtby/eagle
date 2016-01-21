28567b7fd71ed25bf2cd68d5943a92f7791469de
yuzixun <yzxun@qq.com>
2016-01-21 10:31:57 +0800

    add log

--- app/models/point_history.rb
+++ app/models/point_history.rb
@@ -28,8 +28,8 @@ class PointHistory < ActiveRecord::Base
     return if PointHistory.proxy(month: month).first.present? && interval*60 > Time.now - PointHistory.proxy(month: month).first.try(:created_at)
     
     Point.all.each do |point|
-      logger.info "point is #{point.name}"
-      PointHistory.proxy(month: month).create(point_name: point.name, point_value: point.value, point: point, device_id: point.device.id)
+      logger.info "point is #{point.name}, device is #{device.name}"
+      PointHistory.proxy(month: month).create(point_name: point.name, point_value: point.value, point: point, device: point.try(:device))
     end
     nil
   end
