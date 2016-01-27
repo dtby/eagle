a136800c2b49297aad6f486310300d360e3c4aa6
yuzixun <yzxun@qq.com>
2016-01-27 18:03:54 +0800

    add virtual point mdodel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127070047) do
+ActiveRecord::Schema.define(version: 20160127094222) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -211,6 +211,11 @@ ActiveRecord::Schema.define(version: 20160127070047) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["phone"], name: "index_users_on_phone", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "virtual_points", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "alarm_histories", "points"
   add_foreign_key "alarms", "devices"
