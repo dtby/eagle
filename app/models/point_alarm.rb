d3431b7936b6945f458ff56b5f6ad17f1d4bef87
yuzixun <yzxun@qq.com>
2016-02-02 16:52:58 +0800

    fix

--- app/models/point_alarm.rb
+++ app/models/point_alarm.rb
@@ -38,7 +38,7 @@ class PointAlarm < ActiveRecord::Base
   after_update :update_alarm_history, if: "self.is_checked_changed?"
   after_create :generate_alarm_history
 
-  default_scope { where.not(state: nil) }
+  default_scope { where(state: 1) }
 
   #参数point_index
   #返回单个point的id
