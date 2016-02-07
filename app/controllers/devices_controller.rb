31c009a9d85bb9e5cfcf494e7501bda826547230
yuzixun <yzxun@qq.com>
2016-02-07 13:55:10 +0800

    add log

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -30,6 +30,7 @@ class DevicesController < BaseController
   end
 
   def show
+    logger.info "request.format.html? is #{request.format.html?}"
     if request.format.html?
       @room = Room.where(id: params[:room_id]).first
       @device = Device.includes(:points).where(id: params[:id]).first
