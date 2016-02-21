a873b6c5b9c7c76f42bc51ccd2657bd4b2900df7
yuzixun <yzxun@qq.com>
2016-02-21 13:03:59 +0800

    optimize

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -62,7 +62,7 @@ class DevicesController < BaseController
         devices.each do |device|
           @point_values[device.try(:id)] = {}
           case sub_sys_name
-          when "温湿度系统"
+          when "温湿度系统", "消防系统"
             points = device.try(:points)
             points.each do |point|
               @point_values[device.try(:id)][point.name] = point.value
