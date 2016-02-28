e476d0c65e25f787b6f04244639122fb5a80e599
yuzixun <yzxun@qq.com>
2016-02-28 20:29:06 +0800

    add admin authtication

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -18,7 +18,7 @@
 #  name                   :string(255)      default(""), not null
 #  phone                  :string(255)      default(""), not null
 #  authentication_token   :string(255)
-#
+# 
 # Indexes
 #
 #  index_admins_on_authentication_token  (authentication_token)
@@ -31,14 +31,14 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable 
-         # authentication_keys: [:phone]
-  # acts_as_token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:phone]
+  acts_as_token_authenticatable
 
   validates :phone, :email, :name, presence: true
   validates :phone, format: { with: /\A(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}\z/, message: "请输入正确的手机号码" }
 
-  attr_accessor :login
+  # attr_accessor :login
 
   #判断是否需要更新密码
   def update_admin(params)
