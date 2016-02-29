565d4b1a5493732207ff1f1c65a1473ef6c2b979
xiaocuixt <cjw624923@126.com>
2016-02-29 17:42:19 +0800

    remove comment

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -38,7 +38,7 @@ class Admin < ActiveRecord::Base
   validates :phone, :email, :name, presence: true
   validates :phone, format: { with: /\A(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}\z/, message: "请输入正确的手机号码" }
 
-  # attr_accessor :login
+  attr_accessor :login
 
   #判断是否需要更新密码
   def update_admin(params)
