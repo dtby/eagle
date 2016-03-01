d011f0bf3d65d9d3074f802b9f89deb2439e5ed1
yuzixun <yzxun@qq.com>
2016-03-01 16:53:56 +0800

    fix

--- app/models/point.rb
+++ app/models/point.rb
@@ -79,7 +79,7 @@ class Point < ActiveRecord::Base
       if state != point_alarm.state
         puts "DigitalAlarm size is #{PointAlarm.is_warning_alarm.size}, #{da.PointID}, #{point_alarm.state}  => #{state}"
         update_time = DateTime.new(da.ADate.year, da.ADate.month, da.ADate.day, da.ATime.hour,da.ATime.min, da.ATime.sec)
-        point_alarm.update(state: state, comment: dp.Comment, 
+        point_alarm.update(state: state, comment: dp.try(:Comment), 
           is_checked: (state.to_i == 0), updated_at: update_time, alarm_type: 1, 
           room_id: point.try(:device).try(:room).try(:id), 
           device_id: point.try(:device).try(:id), 
@@ -100,7 +100,7 @@ class Point < ActiveRecord::Base
       if state != point_alarm.state
         puts "AnalogAlarm size is #{PointAlarm.is_warning_alarm.size}, #{aa.PointID}, AnalogAlarm #{point_alarm.state}  => #{state}"
         update_time = DateTime.new(aa.ADate.year, aa.ADate.month, aa.ADate.day, aa.ATime.hour,aa.ATime.min, aa.ATime.sec)
-        point_alarm.update(state: state, comment: dp.Comment, 
+        point_alarm.update(state: state, comment: dp.try(:Comment), 
           is_checked: (state == 0), updated_at: update_time, alarm_type: 0,
           room_id: point.try(:device).try(:room).try(:id), 
           device_id: point.try(:device).try(:id), 
