904aa68e34e4434f88665c860399fb99283800a9
shtzr840329 <shtzr840329@gmail.com>
2016-03-24 20:53:07 +0800

    add pue

---
+++ app/controllers/v2/rooms_controller.rb
@@ -0,0 +1,24 @@
+class V2::RoomsController < BaseController
+  acts_as_token_authentication_handler_for User, only: [:show]
+  before_action :authenticate_user!, only: [:show, :pue]
+  before_action :set_room, only: [:show, :pue]
+  respond_to :json
+
+  def show
+
+  end
+
+  def pue
+    @pue_cache = $redis.lrange "pue_#{@room.id}", 0, 5
+    
+  end
+
+  private
+  def room_params
+		params.require(:room).permit(:id)
+	end
+
+  def set_room
+    @room = Room.where(id: params[:id]).first
+  end
+end
