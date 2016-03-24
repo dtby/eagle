5c89ddff9edfbc6ec6c879e05ae2cd53a71b9796
shtzr840329 <shtzr840329@gmail.com>
2016-03-24 16:41:18 +0800

    write pue to cache

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,15 +1,21 @@
 class Schedule
-  
+
   # for PUE point's history value
   # Schedule.point_history
   def self.point_history
     points = Point.where(name: "PUE")
     points.each do |point|
-      caches = ($redis.hget "eagle_schedule_point_history", point.point_index) || (["0"]*24).join("-")
-      values = caches.split("-")
-      values << (point.value || "0")
-      values.shift
-      $redis.hset "eagle_schedule_point_history", point.point_index, values.join("-")
+      device = Device.find_by_id point.device_id
+      item = MultiJson.load($redis.lindex "pue_#{device.room.id}", 0)
+      datetime = DateTime.parse(item['datetime']).to_i
+      if ((point.created_at.to_i - datetime) / 3600) > 1
+        $redis.lpush "pue_#{device.room.id}", {datetime: point.created_at.strftime("%Y-%m-%d %H:00:00"), value: point.value }.to_json
+      end
+      # caches = ($redis.hget "eagle_schedule_point_history", point.point_index) || (["0"]*24).join("-")
+      # values = caches.split("-")
+      # values << (point.value || "0")
+      # values.shift
+      # $redis.hset "eagle_schedule_point_history", point.point_index, values.join("-")
     end
   end
 
