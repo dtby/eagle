39bbf01cd80b1bea8296f16a76f28905d100959d
yuzixun <yzxun@qq.com>
2016-03-26 11:26:37 +0800

    modified the inge info

--- config/initializers/xinge.rb
+++ config/initializers/xinge.rb
@@ -1,8 +1,8 @@
 require 'xinge'
 Xinge.configure do |config|    
-  config[:android_accessId] = 2100190140   
-  config[:android_secretKey] = '42bb9334bdb2cab9d677eed0ca6d4caf'     
-  config[:ios_accessId] = 2200120442  
-  config[:ios_secretKey] = '1d5677036156dd7124493acc62783de0'   
+  config[:android_accessId] = 2100189459
+  config[:android_secretKey] = '938abcacba4a963b10cfac167bbd211b'     
+  config[:ios_accessId] = 2200189460  
+  config[:ios_secretKey] = 'bc60a87a888800a9167a84ad906e41ef'   
   config[:env] = Rails.env # if you are not in a rails app, you can set it config[:env]='development' or config[:env]='production', it is 'development' default.
 end
