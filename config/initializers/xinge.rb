85e25551d1c7c7558dc173a29e1b7577b39b0f37
yuzixun <yzxun@qq.com>
2016-03-30 13:26:05 +0800

    fix

--- config/initializers/xinge.rb
+++ config/initializers/xinge.rb
@@ -2,7 +2,7 @@ require 'xinge'
 Xinge.configure do |config|    
   config[:android_accessId] = 2100189459
   config[:android_secretKey] = '938abcacba4a963b10cfac167bbd211b'     
-  config[:ios_accessId] = 2200189460  
-  config[:ios_secretKey] = 'bc60a87a888800a9167a84ad906e41ef'   
+  config[:ios_accessId] = 2200190139  
+  config[:ios_secretKey] = '148502c288e64f538e4b528fc5f59f23'   
   config[:env] = Rails.env # if you are not in a rails app, you can set it config[:env]='development' or config[:env]='production', it is 'development' default.
 end
