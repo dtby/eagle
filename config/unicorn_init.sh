94eec04890f1d237e671420f51e629ff70ab3b70
OnlyCZY <853852998@qq.com>
2016-04-07 17:58:53 +0800

    fix

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/www/deploy/eagle
+APP_ROOT=/home/deploy/eagle
 #换成自己项目的路径
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
