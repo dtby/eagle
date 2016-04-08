0b08a013b6c5146032fd3c994c2f5945beb4b63b
yuzixun <yzxun@qq.com>
2016-04-08 21:45:27 +0800

    fix

--- app/jobs/notification_send_job.rb
+++ app/jobs/notification_send_job.rb
@@ -11,11 +11,11 @@ class NotificationSendJob < ActiveJob::Base
     point_alarm = PointAlarm.find_by(id: point_alarm_id)
     return unless point_alarm.present?
 
-    begin
+    # begin
       notification_to_app point_alarm
-    rescue Exception => e
-      puts "notification_to_app exception is #{e}"
-    end
+    # rescue Exception => e
+    #   puts "notification_to_app exception is #{e}"
+    # end
 
     begin
       notification_to_wechat point_alarm
