7b32112ff61a161b007650490c54b694f59773ab
fanwei <fanwei@netfarmer.com.cn>
2016-04-10 11:52:52 +0800

    admin after sign in

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -27,8 +27,8 @@ class Admins::SessionsController < Devise::SessionsController
     new_admin_session_path
   end
 
-  # def after_sign_in_path_for(resource_or_scope)
-  #   admin_root_path
-  # end
+  def after_sign_in_path_for(resource_or_scope)
+    admin_root_path
+  end
 
 end
