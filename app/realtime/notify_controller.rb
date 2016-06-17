4fa9114bc82ea399512180d351be6a23719f1bb3
shtzr840329 <shtzr840329@gmail.com>
2016-06-17 16:27:02 +0800

    fix

--- app/realtime/notify_controller.rb
+++ app/realtime/notify_controller.rb
@@ -2,14 +2,14 @@ class NotifyController < FayeRails::Controller
 
   channel '/notify/**' do
     monitor :subscribe do
-      # puts "Client #{client_id} subscribed to #{channel}"
+      puts "Client #{client_id} subscribed to #{channel}"
       matcher = /alarms_(\d+)/.match(channel)
       if matcher and matcher[1].present?
         $redis.hset 'subscribe_alarm_phone', matcher[1], 1
       end
     end
     monitor :unsubscribe do
-      # puts "Client #{client_id} unsubscribed from #{channel}"
+      puts "Client #{client_id} unsubscribed from #{channel}"
       matcher = /alarms_(\d+)/.match(channel)
       if matcher and matcher[1].present?
         $redis.hdel 'subscribe_alarm_phone', matcher[1]
