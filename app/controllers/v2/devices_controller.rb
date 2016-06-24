1099159eb1ec94807b5361694d0ed2c5e58e9406
shtzr840329 <shtzr840329@gmail.com>
2016-06-24 15:50:11 +0800

    fix

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -1,5 +1,6 @@
 class V2::DevicesController < BaseController
   acts_as_token_authentication_handler_for User, only: [:show]
+  
   def show
     @device = Device.find_by(id: params[:id])
     @points = @device.try(:points).try(:order, 'name')#.try(:to_a)
